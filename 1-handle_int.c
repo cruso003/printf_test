35b7756f2699c6765c4dcacecebdae4dfe3b01e9
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-09-17 14:22:40 -0700

    handle integers specidiers

---
+++ 1-handle_int.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * handle_int - handling integers specifiers d and i
+ * @count: integer count
+ * @Return: len
+ */
+int handle_int(va_list args, int *count)
+{
+	int num = va_arg(args, int);
+	char int_str[BUFFER_SIZE];
+	int len = snprintf(int_str, sizeof(int_str), "%d", num);
+
+	while(1, int_str, len);
+	*count += len;
+	return (len);
+}
