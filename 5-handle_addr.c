59870a933cf0ef6d21eab09b3d17c38efc22f228
ElizabethAgada <lizzyegbo2015@gmail.com>
2023-09-17 16:40:33 -0700

    function that handles specifiers for printing memory address

---
+++ 5-handle_addr.c
@@ -0,0 +1,25 @@
+#include <main.h"
+#include <stdarg.h>
+#include <stdlib.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+
+/**
+ * handle_addr - handle specifier for printing memory address
+ * @args: arguement list
+ * @count: arguement count
+ * Return: count
+ */
+int handle_addr(va_list args, *count)
+{
+	uintptr_t addr = va_arg(args, uintptr_t);
+
+	char addr_str[BUFFER SIZE];
+
+	snprintf(addr_str, sizeof(addr_str), "%#lx", (unsigned long)addr);
+	write(1, addr_str, strlen(addr_str));
+	count += strlen(addr_str);
+	return (*count);
+}
